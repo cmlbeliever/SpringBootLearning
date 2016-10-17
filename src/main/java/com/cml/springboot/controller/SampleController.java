ac5e3e9530575ccd1f193049b2898cbe3086e4d1
menglin.chen <menglin.chen>
2016-10-17 15:35:52 +0800

    add junit test

--- src/main/java/com/cml/springboot/controller/SampleController.java
+++ src/main/java/com/cml/springboot/controller/SampleController.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller()
 @EnableAutoConfiguration
 public class SampleController {
 
