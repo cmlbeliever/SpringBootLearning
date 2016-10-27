ddb71b2b0f06d0d78832651b1d71fa16f5418a59
menglin.chen <menglin.chen>
2016-10-27 14:50:17 +0800

    update

--- src/main/java/com/cml/springboot/framework/mybatis/MybatisConfig.java
+++ src/main/java/com/cml/springboot/framework/mybatis/MybatisConfig.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,7 @@ public class MybatisConfig {
 
 	@Value("${db.mybatis.type-handler-package}")
 	private String typeHandlerPackage;
-	
+
 	@Value("${db.mybatis.mapper-locations}")
 	private String mapperLocations;
 
