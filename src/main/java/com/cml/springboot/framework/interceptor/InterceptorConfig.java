0b510815076a881f41e026516366b19b26fa3c6f
menglin.chen <menglin.chen>
2016-10-31 18:09:33 +0800

    update

--- src/main/java/com/cml/springboot/framework/interceptor/InterceptorConfig.java
+++ src/main/java/com/cml/springboot/framework/interceptor/InterceptorConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
@@ -13,22 +15,32 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter {
 	@Autowired
 	private ParamInterceptor paramInterceptor;
 
+	/**
+	 * 1、 extends WebMvcConfigurationSupport 2、重写下面方法; setUseSuffixPatternMatch
+	 * : 设置是否是后缀模式匹配，如“/user”是否匹配/user.*，默认真即匹配； setUseTrailingSlashMatch :
+	 * 设置是否自动后缀路径模式匹配，如“/user”是否匹配“/user/”，默认真即匹配；
+	 */
+	// @Override
+	// public void configurePathMatch(PathMatchConfigurer configurer) {
+	// configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(true);
+	// }
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(paramInterceptor).addPathPatterns("/*");
 		super.addInterceptors(registry);
 	}
 
-//	@Override
-//	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-//
-//		ThreadPoolTaskExecutor ex = new ThreadPoolTaskExecutor();
-//		ex.setCorePoolSize(5);
-//		ex.setMaxPoolSize(500);
-//
-//		configurer.setTaskExecutor(ex);
-//
-//		System.out.println("====InterceptorConfig.configureAsyncSupport==================");
-//	}
+	// @Override
+	// public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+	//
+	// ThreadPoolTaskExecutor ex = new ThreadPoolTaskExecutor();
+	// ex.setCorePoolSize(5);
+	// ex.setMaxPoolSize(500);
+	//
+	// configurer.setTaskExecutor(ex);
+	//
+	// System.out.println("====InterceptorConfig.configureAsyncSupport==================");
+	// }
 
 }
