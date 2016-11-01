725fe24b943b7eb55f1fdb4dd35e33d57b6a5446
menglin.chen <menglin.chen>
2016-11-01 17:18:52 +0800

    update

--- src/main/java/com/cml/springboot/framework/AsyncTaskConfig.java
+++ src/main/java/com/cml/springboot/framework/AsyncTaskConfig.java
@@ -2,7 +2,10 @@ package com.cml.springboot.framework;
 
 import java.util.concurrent.Executor;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -10,12 +13,21 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class AsyncTaskConfig implements AsyncConfigurer {
 
+	protected static Log log = LogFactory.getLog(AsyncTaskConfig.class);
+
+	@Value("${async.task.config.corePoolSize}")
+	private Integer corePoolSize;
+	@Value("${async.task.config.maxPoolSize}")
+	private Integer maxPoolSize;
+
 	@Override
 	public Executor getAsyncExecutor() {
-		System.out.println("====AsyncTaskConfig.getAsyncExecutor==================");
+
+		log.info("<<<AsyncTaskConfig>>>====>corePoolSize:" + corePoolSize + ",maxPoolSize:" + maxPoolSize);
+
 		ThreadPoolTaskExecutor ex = new ThreadPoolTaskExecutor();
-		ex.setCorePoolSize(5);
-		ex.setMaxPoolSize(500);
+		ex.setCorePoolSize(corePoolSize);
+		ex.setMaxPoolSize(maxPoolSize);
 		ex.initialize();
 		return ex;
 	}
--- src/main/java/com/cml/springboot/framework/interceptor/InterceptorConfig.java
+++ src/main/java/com/cml/springboot/framework/interceptor/InterceptorConfig.java
@@ -2,12 +2,8 @@ package com.cml.springboot.framework.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
