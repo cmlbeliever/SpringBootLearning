997ef7d3d83e900e9483c9a7adf8eecd335d51a0
menglin.chen <menglin.chen>
2016-11-01 18:24:49 +0800

    update

--- src/main/java/com/cml/springboot/sample/controller/UserController.java
+++ src/main/java/com/cml/springboot/sample/controller/UserController.java
@@ -3,11 +3,13 @@ package com.cml.springboot.sample.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cml.springboot.framework.Configuration;
 import com.cml.springboot.framework.response.BaseResponse;
 import com.cml.springboot.framework.util.LogUtil;
 
@@ -17,13 +19,22 @@ public class UserController {
 
 	protected static Log LOG = LogFactory.getLog(UserController.class);
 
-	@RequestMapping
+	@RequestMapping("/login")
 	@ResponseBody
 	public BaseResponse login(@RequestParam(required = true) String username,
-			@RequestParam(required = true) String password, BindingResult result) {
-		if (result.hasErrors()) {
-			LOG.info(LogUtil.formatControllerLog(this, "有错误信息需要处理"));
-		}
-		return null;
+			@RequestParam("password") String password) {
+
+		// if (result.hasErrors()) {
+		// LOG.info(LogUtil.formatControllerLog(this, "有错误信息需要处理"));
+		// }
+		return new BaseResponse(Configuration.Status.STATUS_OK, "登录成功");
+	}
+
+	@ExceptionHandler({ MissingServletRequestParameterException.class })
+	@ResponseBody
+	public BaseResponse exception(MissingServletRequestParameterException e) {
+		LOG.info(LogUtil.formatControllerLog(this, e.getMessage()));
+		throw new RuntimeException(e);
+		// return new BaseResponse(Configuration.Status.STATUS_OK, "异常处理");
 	}
 }
