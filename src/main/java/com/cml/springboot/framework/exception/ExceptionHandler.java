015aa092dcfe6683755e7f8b37e802625c14e3d1
menglin.chen <menglin.chen>
2016-11-03 16:49:22 +0800

    update exception

--- src/main/java/com/cml/springboot/framework/exception/ExceptionHandler.java
+++ src/main/java/com/cml/springboot/framework/exception/ExceptionHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
@@ -19,20 +20,29 @@ public class ExceptionHandler extends DefaultHandlerExceptionResolver {
 
 	private static Log log = LogFactory.getLog(ExceptionHandler.class);
 
+	@Value("${exception.response.filter:false}")
+	private boolean filterErrorMsg;
+	@Value("${exception.response.messasge:errors occurs}")
+	private String defaultErrorMsg;
+	@Value("${exception.response.contentType:application/json}")
+	private String contentType;
+
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception ex) {
-		
 
-		log.info("====exception===================================>" + ex.getMessage());
+		log.warn("<<< exception occurs >>>", ex);
 
 		ObjectMapper mapper = new ObjectMapper();
-		response.setContentType("application/json");
+		
 		mapper.setSerializationInclusion(Include.NON_NULL);
-		BaseResponse responseBean = new BaseResponse(Configuration.Status.STATUS_INVALID_TOKEN, ex.getMessage());
+
+		BaseResponse responseBean = new BaseResponse(Configuration.Status.STATUS_FAIL,
+				filterErrorMsg ? defaultErrorMsg : ex.getMessage());
 		try {
 			String message = mapper.writeValueAsString(responseBean);
 			response.reset();
+			response.setContentType(contentType);
 			response.getOutputStream().write(message.getBytes());
 			response.getOutputStream().flush();
 		} catch (Exception e) {
