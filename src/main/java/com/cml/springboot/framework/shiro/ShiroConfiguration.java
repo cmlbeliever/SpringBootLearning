372337ba85b6938d16fcc605700e7ff961ffaac7
menglin.chen <menglin.chen>
2016-11-14 17:18:19 +0800

    update

--- src/main/java/com/cml/springboot/framework/shiro/ShiroConfiguration.java
+++ src/main/java/com/cml/springboot/framework/shiro/ShiroConfiguration.java
@@ -1,14 +1,19 @@
 package com.cml.springboot.framework.shiro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.remoting.SecureRemoteInvocationExecutor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 public class ShiroConfiguration {
@@ -62,6 +67,22 @@ public class ShiroConfiguration {
 		shiroFilter.setFilterChainDefinitionMap(properties.getFilters());
 
 		return shiroFilter;
+	}
+
+	@Bean
+	public FilterRegistrationBean shiroFilter() {
+		
+		FilterRegistrationBean filterBean = new FilterRegistrationBean();
+
+		List<String> urlPatterns = new ArrayList<String>();
+		urlPatterns.add("/*");
+		filterBean.setUrlPatterns(urlPatterns);
+
+		DelegatingFilterProxy filter = new DelegatingFilterProxy();
+		filter.setTargetFilterLifecycle(true);
+		filterBean.setFilter(filter);
+
+		return filterBean;
 	}
 
 }
