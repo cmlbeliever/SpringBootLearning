cfa360dd043d7ed41f4ac6e8c8d934feb2d039c6
admin <865517964@qq.com>
2017-02-19 13:17:50 +0800

    add comment generator

---
+++ src/main/java/com/cml/springboot/sample/mbg/CommentGenerate.java
@@ -0,0 +1,102 @@
+package com.cml.springboot.sample.mbg;
+
+import java.util.Properties;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.InnerClass;
+import org.mybatis.generator.api.dom.java.InnerEnum;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+public class CommentGenerate implements CommentGenerator {
+
+	@Override
+	public void addConfigurationProperties(Properties properties) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addFieldComment(Field field, IntrospectedTable introspectedTable,
+			IntrospectedColumn introspectedColumn) {
+		if (introspectedColumn.getRemarks() != null && !introspectedColumn.getRemarks().equals("")) {
+			field.addJavaDocLine("/**");
+			field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+			field.addJavaDocLine(" */");
+		}
+	}
+
+	@Override
+	public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addGetterComment(Method method, IntrospectedTable introspectedTable,
+			IntrospectedColumn introspectedColumn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addSetterComment(Method method, IntrospectedTable introspectedTable,
+			IntrospectedColumn introspectedColumn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addJavaFileComment(CompilationUnit compilationUnit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addComment(XmlElement xmlElement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addRootComment(XmlElement rootElement) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
