6fdf73cdca1f97491efb6a6d2de44992b7fbe01f
menglin.chen <menglin.chen>
2017-04-18 17:24:36 +0800

    add advisor code

--- src/main/java/com/cml/springboot/BootApplication.java
+++ src/main/java/com/cml/springboot/BootApplication.java
@@ -1,5 +1,6 @@
 package com.cml.springboot;
 
+import org.springframework.aop.support.RegexpMethodPointcutAdvisor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,6 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 @Configuration
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
@@ -28,5 +34,36 @@ public class BootApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BootApplication.class, args);
+//
+//		TransactionInterceptor interceptor = new TransactionInterceptor();
+//		interceptor.setTransactionManager(null);
+//
+//		DefaultTransactionAttribute attribute = new DefaultTransactionAttribute();
+//		attribute.setPropagationBehavior(Propagation.REQUIRED.value());
+//		attribute.rollbackOn(new Exception());
+//
+//		// <tx:method name="*" rollback-for="java.lang.Throwable" />
+//		NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+//		source.addTransactionalMethod("update*", attribute);
+//
+//		interceptor.setTransactionAttributeSources(new TransactionAttributeSource[] { source });
+//
+//		RegexpMethodPointcutAdvisor advisor = new RegexpMethodPointcutAdvisor();
+//		// advisor.setPatterns("*set", "update*");
+//		advisor.setPattern("execution(* *..service..*Service.*(..))");
+//		advisor.setAdvice(interceptor);
+
+		// <aop:config> 配置
+		// BeanFactoryTransactionAttributeSourceAdvisor advisor = new
+		// BeanFactoryTransactionAttributeSourceAdvisor();
+		// Pointcut pointCut = new StaticMethodMatcherPointcutAdvisor() {
+		// @Override
+		// public boolean matches(Method method, Class<?> targetClass) {
+		// return false;
+		// }
+		// };
+
+		// DefaultAopProxyFactory proxy=new DefaultAopProxyFactory();
+
 	}
 }
--- src/main/java/com/cml/springboot/framework/transaction/TransactionService.java
+++ src/main/java/com/cml/springboot/framework/transaction/TransactionService.java
@@ -3,11 +3,17 @@ package com.cml.springboot.framework.transaction;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.springframework.aop.support.RegexpMethodPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 @Configuration
 @EnableTransactionManagement()
@@ -19,6 +25,31 @@ public class TransactionService {
 	@Bean(name = "txManager")
 	public PlatformTransactionManager txManager() {
 		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean
+	public RegexpMethodPointcutAdvisor pointTransaction(PlatformTransactionManager tx) {
+		TransactionInterceptor interceptor = new TransactionInterceptor();
+		interceptor.setTransactionManager(tx);
+
+		DefaultTransactionAttribute attribute = new DefaultTransactionAttribute();
+		attribute.setPropagationBehavior(Propagation.REQUIRED.value());
+		attribute.rollbackOn(new Exception());
+		attribute.setReadOnly(true);
+
+		// <tx:method name="*" rollback-for="java.lang.Throwable" />
+		NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+		source.addTransactionalMethod("update*", attribute);
+
+		interceptor.setTransactionAttributeSources(new TransactionAttributeSource[] { source });
+
+		RegexpMethodPointcutAdvisor advisor = new RegexpMethodPointcutAdvisor();
+		// advisor.setPatterns("*set", "update*");
+		// advisor.setPattern("execution(* *..service..*Service.*(..))");
+		advisor.setPattern(".*service\\..*Service.*");
+		advisor.setAdvice(interceptor);
+		return advisor;
+
 	}
 
 }
