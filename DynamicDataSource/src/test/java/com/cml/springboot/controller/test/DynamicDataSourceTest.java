b5c1493757d7b1e14e55ff4c74ba36bc245ba76b
menglin.chen <menglin.chen>
2017-09-29 09:45:31 +0800

    测试代码添加

---
+++ DynamicDataSource/src/test/java/com/cml/springboot/controller/test/DynamicDataSourceTest.java
@@ -0,0 +1,28 @@
+package com.cml.springboot.controller.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cml.springboot.BootApplication;
+import com.cml.springboot.sample.bean.User;
+import com.cml.springboot.sample.service.UserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BootApplication.class)
+public class DynamicDataSourceTest {
+
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void testLogin() throws Exception {
+		User user = new User();
+		user.setUsername("admin");
+		user.setPassword("111111");
+		User loginUser = userService.login(user);
+		System.out.println("登录结果：" + loginUser);
+	}
+}
