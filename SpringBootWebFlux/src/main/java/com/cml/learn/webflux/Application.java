c31227218f4261b87365a87dfa80af183a217246
cml <menglin.chen>
2017-11-15 17:24:31 +0800

    webflux init

---
+++ SpringBootWebFlux/src/main/java/com/cml/learn/webflux/Application.java
@@ -0,0 +1,22 @@
+package com.cml.learn.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication()
+@Controller
+@RequestMapping
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String index() {
+		return "hello world";
+	}
+}
