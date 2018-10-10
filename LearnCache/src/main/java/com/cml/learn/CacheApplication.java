fe71208006926a87bd9da918b0c40ba9dd37268b
admin <865517964@qq.com>
2018-10-10 20:57:57 +0800

    cache添加

---
+++ LearnCache/src/main/java/com/cml/learn/CacheApplication.java
@@ -0,0 +1,9 @@
+package com.cml.learn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CacheApplication {
+}
